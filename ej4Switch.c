1e7c59fae5f71ad2ba613128fd651e0f2bca7d76
Erick-C3 <81482790+Erick-C3@users.noreply.github.com>
2021-06-17 09:51:58 -0300

    Add files via upload

---
+++ ej4Switch.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/*
+	
+	Como pronunciar el conjuro Wingardium leviosa
+
+
+	Permitir al usuario ingresar una opcion para completar el conjuro con un conjunto de caracteres especifico dado
+
+	La opcion puede ser numerica o de caracter, al elegir la opcion se debera asignar un conjunto de caracteres
+	correspondientes a la misma y mostrar el resultado en consola
+
+	Conjunto de caracteres:
+		- constantes desde CONJURO_1 hasta CONJURO_8
+
+	Conjuro a completar:
+		- "Win-GAR-di-um %s-i-O-sa"
+
+	ej:
+		si el usuario ingresa la opcion 3 el conjuro deberia quedar como "Win-GAR-di-um di-i-O-sa" 
+
+
+	extra: informar si la opcion fue correcta o no
+	
+
+*/
+
+//NO MODIFICAR
+#define CONJURO_1 "Win"
+#define CONJURO_2 "GAR"
+#define CONJURO_3 "di"
+#define CONJURO_4 "um"
+
+#define CONJURO_5 "lev"
+#define CONJURO_6 "i"
+#define CONJURO_7 "O"
+#define CONJURO_8 "sa"
+//NO MODIFICAR
+
+//OPCIONAL para usar con el switch
+#define OPC_1  1
+#define OPC_2  2
+#define OPC_3  3
+#define OPC_4  4
+#define OPC_5  5
+#define OPC_6  6
+#define OPC_7  7
+#define OPC_8  8
+//OPCIONAL
+
+int main(){
+
+
+
+	return 0;
+}
